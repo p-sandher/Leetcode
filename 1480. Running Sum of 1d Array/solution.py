48f2f14778a395714922b44c31da821f7517aeb3
Puneet Penelope Sandher <87533668+p-sandher@users.noreply.github.com>
2022-10-03 11:22:35 -0400

    Update solution.py

--- 1480. Running Sum of 1d Array/solution.py
+++ 1480. Running Sum of 1d Array/solution.py
@@ -3,3 +3,4 @@ class Solution:
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i-1] #replace each value in array with new added value
         return nums
+#sorta like fibonacci for one term
