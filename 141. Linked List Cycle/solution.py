d74c061f303a99e084d55c18e2f134c381af9684
Puneet Penelope Sandher <87533668+p-sandher@users.noreply.github.com>
2022-10-05 12:47:05 -0400

    Create solution.py

---
+++ 141. Linked List Cycle/solution.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        #fast pointer (+2) and slow pointer (+1)
+        #fast pointer, if it ends youll reach a NULL
+        #if they meet, that means there is a loop
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
