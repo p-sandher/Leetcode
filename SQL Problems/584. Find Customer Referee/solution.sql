1ad50144170ef66ee1f0f9fae12ba6a1a6ba7618
Puneet Sandher <87533668+p-sandher@users.noreply.github.com>
2022-12-25 20:29:36 -0500

    Create solution.sql

---
+++ SQL Problems/584. Find Customer Referee/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+#Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+OR referee_id IS NULL;
